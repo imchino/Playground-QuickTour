39061882e2b64d9ecfc02c0554d473bcebfe988d
imchino <chino618@me.com>
2020-05-27 23:37:18 +0900

    Add Playground Book.

---
+++ QuickTour.playground/Sources/helper.swift
@@ -0,0 +1,15 @@
+public func justDoIt() -> String {
+    return "You will see the results here."
+}
+
+public func pressShiftAndReturnKey() -> String {
+    return "The code has been executed and the cursor has moved to the next line."
+}
+
+public func pressShiftAndReturnKeyAgain() -> String {
+    return "The code has been revise."
+}
+
+public func pressShiftAndCommandAndReturnKey() -> String {
+    return "All the code has been executed."
+}
